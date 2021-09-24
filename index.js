573844d53cd1667e59475694425038ec5082bb87
devHikary <wrkkarina@gmail.com>
2021-09-24 07:16:06 -0300

    inicialização projeto

---
+++ index.js
@@ -0,0 +1,6 @@
+function Converter(){
+  var valorElemento = document.querySelector('#valor');
+  var valor = valorElemento.value
+  var valorEmDolar = parseFloat(valor)
+  console.log(valorEmDolar);
+}
